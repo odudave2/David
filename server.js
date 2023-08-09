6c64e2d2ed09e78bb5eb439fc39a982979b03412
David Oduwole <odudave2@gmail.com>
2023-08-09 14:30:02 -0400

    Initial commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 /*********************************************************************************
- *  * WEB700 – Assignment 05 * I declare that this assignment is my own work in 
+ *  * WEB700 – Assignment 06 * I declare that this assignment is my own work in 
  * accordance with Seneca Academic Policy. No part * of this assignment has been 
  * copied manually or electronically from any other source * (including 3rd party
  *  web sites) or distributed to other students. *
- * Name: __David Oduwole___ Student ID: __185731213__ Date: ____July 27, 2023__ *
- * Online (Cyclic) Link:__https://determined-blue-mackerel.cyclic.app__ * *
+ * Name: __David Oduwole___ Student ID: __185731213__ Date: ____August 9, 2023__ *
+ * Online (Cyclic) Link:__https://muddy-eel-trunks.cyclic.app__ * *
  * *******************************************************************************/
 
 
